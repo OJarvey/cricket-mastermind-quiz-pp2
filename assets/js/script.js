002a3f57f408169528e55d7ca0aa936de2a57643
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-06-17 21:49:11 +0100

    Added DOM elements for quiz

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Getters
+const question = document.getElementById('question');
+const answer1 = document.getElementById('answer1');
+const answer2 = document.getElementById('answer2');
+const answer3 = document.getElementById('answer3');
+const answer4 = document.getElementById('answer4');
+const score = document.getElementById('score');
+const playAgain = document.getElementById('playAgain');
+const wrapper = document.getElementById('wrapper');
+const result = document.getElementById('result');
