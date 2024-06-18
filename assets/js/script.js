9438c74a627e2226e446bac998f68c5298a8c18c
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-06-18 22:32:25 +0100

    Initialized four variables to track the state of the quiz game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Getters
+// Getters: These lines get references to HTML elements by their IDs and assign them to variables.
 const question = document.getElementById('question');
 const answer1 = document.getElementById('answer1');
 const answer2 = document.getElementById('answer2');
@@ -8,3 +8,9 @@ const score = document.getElementById('score');
 const playAgain = document.getElementById('playAgain');
 const wrapper = document.getElementById('wrapper');
 const result = document.getElementById('result');
+
+// Initialize variables to track the question number, score, timer, and time left.
+let questionNumber = 0;
+let scoreAmount = 0;
+let timer;
+let timeLeft;
