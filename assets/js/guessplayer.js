d2393fa07e60f44a1d115d66ed7ad2946aede520
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-06-20 00:13:43 +0100

    Added getters targeting the ID's in html

---
+++ assets/js/guessplayer.js
@@ -0,0 +1,12 @@
+// Getters: These lines get references to HTML elements by their IDs and assign them to variables.
+const question = document.getElementById('question');
+const questionImage = document.getElementById('question-image');
+const answer1 = document.getElementById('answer1');
+const answer2 = document.getElementById('answer2');
+const answer3 = document.getElementById('answer3');
+const answer4 = document.getElementById('answer4');
+const score = document.getElementById('score');
+const playAgain = document.getElementById('playAgain');
+const wrapper = document.getElementById('wrapper');
+const timeDisplay = document.getElementById('time');
+const result = document.getElementById('result');
